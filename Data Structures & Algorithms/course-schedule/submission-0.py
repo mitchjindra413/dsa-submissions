eb4eaedf5ff6a1d40d837cedc1ac16b224b2ea05
Mitchell Jindra <65314998+mitchjindra413@users.noreply.github.com>
2026-06-05 13:36:38 -0700

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre_req = defaultdict(list)
+
+        for a, b in prerequisites:
+            pre_req[a].append(b)
+        
+        seen = set()
+
+        def explore(node):
+            if node in seen:
+                return False
+            if pre_req[node] == []:
+                return True
+            
+            seen.add(node)
+            for pr in pre_req[node]:
+                if not explore(pr):
+                    return False
+            seen.remove(node)
+            pre_req[node] = []
+            return True
+
+        for c in range(numCourses):
+            if not explore(c):
+                return False
+        return True
